0c14aadee6f1f6e028d785c0d9dd976925d87a23
Melvin <mjviana057@gmail.com>
2021-12-03 14:51:43 -0800

    Bubble sort; Colt Steele

---
+++ udemy-colt-steele/sorting/bubblesort.js
@@ -0,0 +1,22 @@
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
+
+const bubbleSort = (arr) => {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if(arr[j] > arr[j+1]) {
+        let temp = arr[j+1];
+        arr[j]  = arr[j+1];
+        arr[j+1] = temp;
+        noSwaps = false;
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+}
+
+console.log(bubbleSort([8,1,2,3,4,5,6,7]));
